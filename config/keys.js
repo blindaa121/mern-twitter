7c407c5584404def315661c7c7c1aeffab06bbed
Bryan <blindaa121@gmail.com>
2020-05-09 14:30:17 -0700

    remove keys

--- config/keys.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    mongoURI: 'mongodb+srv://blindaa121:hXPQThiq1uD30Zf0@cluster0-sjine.mongodb.net/test?retryWrites=true&w=majority'
-    //Make sure this is your own unique string
-}
