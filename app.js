668db4ca101d61d3297910f576c97b16b7d00133
Bryan <blindaa121@gmail.com>
2020-05-09 15:04:37 -0700

    create express routes for users and tweets

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const mongoose = require('mongoose');
 const port = process.env.PORT || 5000;
 const app = express();
 const db = require('./config/keys').mongoURI;
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
 
 mongoose
     .connect(db, { useNewUrlParser: true })
@@ -11,3 +13,7 @@ mongoose
 
 app.get("/", (req, res) => res.send("MERN Twitter"));
 app.listen(port, () => console.log(`Server is running on port ${port}`));
+
+// users and tweets routes
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
