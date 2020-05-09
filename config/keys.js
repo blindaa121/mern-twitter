a02d27db52bfbefb7e11b5f4a91234c3ab93fa73
Bryan <blindaa121@gmail.com>
2020-05-09 14:22:02 -0700

    first commit

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://blindaa121:hXPQThiq1uD30Zf0@cluster0-sjine.mongodb.net/test?retryWrites=true&w=majority'
+    //Make sure this is your own unique string
+}
