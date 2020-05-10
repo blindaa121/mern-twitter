320ec5de8d56ececccac2eebe485ac29d117fb87
Bryan <blindaa121@gmail.com>
2020-05-09 18:29:15 -0700

    create private auth route

--- app.js
+++ app.js
@@ -3,14 +3,13 @@ const mongoose = require('mongoose');
 const port = process.env.PORT || 5000;
 const app = express();
 const db = require('./config/keys').mongoURI;
-
+const passport = require('passport');
 
 mongoose
     .connect(db, { useNewUrlParser: true })
     .then(() => console.log("Connected to MongoDB successfully"))
     .catch(err => console.log(err));
 
-app.get("/", (req, res) => res.send("MERN Twitter"));
 app.listen(port, () => console.log(`Server is running on port ${port}`));
 
 //parse the JSON for the frontend
@@ -23,3 +22,7 @@ const users = require("./routes/api/users");
 const tweets = require("./routes/api/tweets");
 app.use("/api/users", users);
 app.use("/api/tweets", tweets);
+
+//passport 
+app.use(passport.initialize());
+require('./config/passport')(passport);
--- routes/api/users.js
+++ routes/api/users.js
@@ -4,6 +4,7 @@ const bcrypt = require('bcryptjs');
 const User = require('../../models/User');
 const jwt = require('jsonwebtoken');
 const keys = require('../../config/keys');
+const passport = require('passport');
 
 router.get("/test", (req, res) => res.json({ msg: "This is the users route" }));
 
@@ -79,5 +80,7 @@ router.post('/login', (req, res) => {
                 });
         });
 });
+
+
 
 module.exports = router;
