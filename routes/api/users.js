b059f74b92faeeb0202827fb6b93dc8ae674ce13
Bryan <blindaa121@gmail.com>
2020-05-09 19:40:42 -0700

    create validations for login and register

--- routes/api/users.js
+++ routes/api/users.js
@@ -81,6 +81,12 @@ router.post('/login', (req, res) => {
         });
 });
 
-
+router.get('/current', passport.authenticate('jwt', {session: false}), (req, res) => {
+    res.json({
+        id: req.user.id,
+        handle: req.user.handle,
+        email: req.user.email
+    });
+})
 
 module.exports = router;
