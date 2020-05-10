e7c35f83dbb81c9a0f1d6993f444e536a8f2d0bf
Bryan <blindaa121@gmail.com>
2020-05-10 11:01:55 -0700

    begin frontend auth

---
+++ frontend/src/components/app.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { AuthRoute, ProtectedRoute } from '../util/route_util';
+import { Switch } from 'react-router-dom';
+import NavBarContainer from './nav/navbar_container';
+
+import MainPage from './main/main_page';
+import LoginFromContainer from './session/login_form_container';
+import SignupFormContainer from './session/signup_form_container';
+
+const App = () => (
+    <div>
+        <NavBarContainer />
+        <Switch>
+            <AuthRoute exact path="/" component={MainPage} />
+            <AuthRoute exact path="/login" component={LoginFromContainer} />
+            <AuthRoute exact path="/signup" component={SignupFormContainer} />
+        </Switch> 
+    </div>
+    
+);
+
+export default App;
