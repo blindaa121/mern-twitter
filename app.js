b9204de9ce2902e0f4439aa6a01e397174860c46
Bryan <blindaa121@gmail.com>
2020-05-10 19:02:36 -0700

    heroku push

--- app.js
+++ app.js
@@ -4,6 +4,14 @@ const port = process.env.PORT || 5000;
 const app = express();
 const db = require('./config/keys').mongoURI;
 const passport = require('passport');
+const path = require('path');
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('frontend/build'));
+    app.get('/', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+    })
+}
 
 mongoose
     .connect(db, { useNewUrlParser: true })
---
+++ config/keys_dev.js
@@ -0,0 +1,5 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://blindaa121:hXPQThiq1uD30Zf0@cluster0-sjine.mongodb.net/test?retryWrites=true&w=majority',
+    secretOrKey: 'qgBODL2Fmb'
+    //Make sure this is your own unique string
+}
